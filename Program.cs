da572145302cd78364c675514ccf692613c7a433
Andrey <andreytelushko@gmail.com>
2022-12-18 12:07:16 +0300

    Center

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Program
+{
+    public static void Center()
+    {
+        string a = Console.ReadLine();
+        if (a.Length == 3)
+        {       
+            Console.WriteLine(a[1]);
+        }
+        else
+        {
+            Console.WriteLine("Не трехзначное число!!!");
+        }
+    }
+    public static void Main()
+    {
+        Center();
+    }
+}
