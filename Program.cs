16e26593466fa695f7e536710dd1d8915ebd032d
Andrey <andreytelushko@gmail.com>
2022-12-18 12:16:33 +0300

    Last

--- Program.cs
+++ Program.cs
@@ -12,8 +12,21 @@
             Console.WriteLine("Не трехзначное число!!!");
         }
     }
+    public static void Last()
+    {
+        string a = Console.ReadLine();
+        if (a.Length ==3)
+        {
+            Console.WriteLine(a[2]);
+        }
+        else
+        {
+            Console.WriteLine("третьей цифры нет!!!");
+        }
+    }
     public static void Main()
     {
         Center();
+        Last();
     }
 }
